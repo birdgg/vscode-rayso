2832eba1126c5e92bf90c41c2f14ebd9ece45c98
Birdgg <birdeggegg@gmail.com>
2023-02-01 01:15:20 +0800

    🐛 read configration

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export const getSettings = (group: string, keys: string[]) => {
   const settings = vscode.workspace.getConfiguration(group, null);
   return keys.reduce((acc, k) => {
     // @ts-ignore
-    if (acc[k] === null) acc[k] = settings.get(k);
+    acc[k] = settings.get(k);
     return acc;
   }, {});
 };
